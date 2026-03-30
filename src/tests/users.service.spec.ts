fdde98680c220651a44053b276c358bc21c02d18
xudargabovqaxramon-creator <xudargabovqaxramon@gmail.com>
2026-03-30 21:48:08 +0500

    init

--- src/tests/users.service.spec.ts
+++ src/tests/users.service.spec.ts
@@ -1,7 +1,9 @@
 import { Test } from "@nestjs/testing";
-import { UsersService } from "src/users/users.service";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { User } from "./../users/entities/user.entity";
+import { UsersService } from "./../users/users.service";
 
-const mockUserRepository = {
+const mockRepo = {
   create: jest.fn(),
   save: jest.fn(),
   find: jest.fn(),
@@ -12,14 +14,75 @@ const mockUserRepository = {
 
 
 describe('UsersService', () => {
-    let usersService: UsersService;
+    let Service: UsersService;
     let repo:any;
 
     beforeEach(async () => {
-        const module = Test.createTestingModule({
+        const module =await Test.createTestingModule({
             providers: [
                 UsersService,
-                {provide: 'UserRepository', useValue: mockUserRepository}]
-        })
+                {provide: getRepositoryToken(User), useFactory: () => mockRepo}
+            ]
+        }).compile();
+
+        Service = module.get<UsersService>(UsersService);
+        repo = module.get(getRepositoryToken(User));
     })
+
+
+    it("create user", async () => {
+        const createUserDto = {name: "qaxramon", email: "khaxramon.com"};
+        const user = {id: 1, ...createUserDto};
+        repo.create.mockReturnValue(user);
+        repo.save.mockResolvedValue(user);
+
+        expect(await Service.create(createUserDto)).toEqual(user);
+    });
+
+
+    it("find all users", async () => {
+        const users = [{id: 1, name: "qaxramon", email: "khaxramon.com"}];
+        repo.find.mockResolvedValue(users);
+        expect(await Service.findAll()).toHaveLength(users.length);
+    });
+
+
+
+    it("find one user", async () => {
+        const user = {id: 1, name: "qaxramon", email: "khaxramon.com"};
+        repo.findOne.mockResolvedValue(user);
+
+        expect(await Service.findOne(1)).toEqual(user); 
+    });
+it("update user", async () => {
+    const oldUser = { id: 1, name: "qaxramon", email: "khaxramon.com" };
+    const updateUserDto = { name: "Kaxa", email: "@khaxramon.com" };
+    const updatedUser = { id: 1, ...updateUserDto };
+
+    repo.findOne.mockResolvedValueOnce(oldUser);
+    repo.save.mockResolvedValue(updatedUser);
+
+    const result = await Service.update(1, updateUserDto);
+
+    // Tekshiramiz
+    expect(result).toEqual(updatedUser);
+    expect(repo.findOne).toHaveBeenCalledWith({ where: { id: 1 } });
+    expect(repo.save).toHaveBeenCalled();
+});
+
+    it("remove user", async () => {
+        const user = {id: 1, name: "qaxramon", email: "khaxramon.com"};
+        repo.findOne.mockResolvedValue(user);
+        repo.remove.mockResolvedValue(undefined);
+
+        await expect(Service.remove(1)).resolves.toBeUndefined();
+    });
+    
+
+
+   it("throw error", async () => {
+    repo.findOne.mockResolvedValue(null);
+
+   await expect(Service.findOne(999)).rejects.toThrow(`User with id 999 not found`);
+   });
 });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { Not, Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
@@ -17,15 +17,23 @@ export class UsersService {
     return await this.userRepository.find();
   }
 
-  async findOne(id: number) {
-    return await this.userRepository.findOne({ where: { id } });
+  async findOne(id: number) { 
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) throw new NotFoundException(`User with id ${id} not found`);
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+     const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) throw new NotFoundException(`User with id ${id} not found`);
+    Object.assign(user, updateUserDto);
+     return  await this.userRepository.save(user); 
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+     const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) throw new NotFoundException(`User with id ${id} not found`);
+
+    return await this.userRepository.remove(user);
   }
 }
